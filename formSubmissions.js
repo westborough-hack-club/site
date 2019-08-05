98ef464d898b621be8f8086c7f9a1330d129057d
Saharsh Yeruva <syeruva2003@gmail.com>
2019-08-05 19:44:32 -0400

    Going to just use Netlify forms for now

--- formSubmissions.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = '<https://script.google.com/macros/s/AKfycbxw1ak7kYrjbrbcZVg_qJ-6yx9akqalplCwWn4bDh23Qa4sKyA/exec>';
-  const form = document.forms['submit-to-google-sheet'];
-
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message));
-  });
